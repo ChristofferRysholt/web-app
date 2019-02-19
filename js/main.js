37752119030a66ba7956198cb28c410a1deeeea5
Asger D.R <eaaasra@students.eaaa.dk>
2019-02-19 10:00:38 +0100

    Asger code

--- js/main.js
+++ js/main.js
@@ -3,39 +3,39 @@
 // ---------- default SPA Web App setup ---------- //
 // hide all pages
 function hideAllPages() {
-  let pages = document.querySelectorAll(".page");
-  for (let page of pages) {
-    page.style.display = "none";
-  }
+    let pages = document.querySelectorAll(".page");
+    for (let page of pages) {
+        page.style.display = "none";
+    }
 }
 
 // show page or tab
 function showPage(pageId) {
-  hideAllPages();
-  console.log(pageId);
-  document.querySelector(`#${pageId}`).style.display = "grid";
-  setActiveTab(pageId);
+    hideAllPages();
+    console.log(pageId);
+    document.querySelector(`#${pageId}`).style.display = "grid";
+    setActiveTab(pageId);
 }
 
 // set default page
 function setDefaultPage(defaultPageName) {
-  if (location.hash) {
-    defaultPageName = location.hash.slice(1);
-  }
-  showPage(defaultPageName);
+    if (location.hash) {
+        defaultPageName = location.hash.slice(1);
+    }
+    showPage(defaultPageName);
 }
 
 // sets active tabbar/ menu item
 function setActiveTab(pageId) {
-  let pages = document.querySelectorAll(".tabbar a");
-  for (let page of pages) {
-    if (`#${pageId}` === page.getAttribute("href")) {
-      page.classList.add("active");
-    } else {
-      page.classList.remove("active");
-    }
+    let pages = document.querySelectorAll(".tabbar a");
+    for (let page of pages) {
+        if (`#${pageId}` === page.getAttribute("href")) {
+            page.classList.add("active");
+        } else {
+            page.classList.remove("active");
+        }
 
-  }
+    }
 }
 
 // ---------- Fetch data from data sources ---------- //
@@ -43,25 +43,25 @@ function setActiveTab(pageId) {
 // Fetches pages json data from headless cms
 
 fetch("http://rysholt.com/wordpress/wp-json/wp/v2/pages?_embed")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(pages) {
-    appendPages(pages);
-  });
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (pages) {
+        appendPages(pages);
+    });
 
 
 // Appends and generate pages
 
 function appendPages(pages) {
-  var menuTemplate = "";
-  for (let page of pages) {
-    addMenuItem(page);
-    addPage(page);
-  }
-  setDefaultPage(pages[0].slug); // selecting the first page in the array of pages
-  getMovies();
-  getCategories();
+    var menuTemplate = "";
+    for (let page of pages) {
+        addMenuItem(page);
+        addPage(page);
+    }
+    setDefaultPage(pages[0].slug); // selecting the first page in the array of pages
+    getMovies();
+    getCategories();
 }
 
 /*
@@ -80,17 +80,19 @@ function addMenuItem(page) {
 
 // appends menu item to the nav menu
 function addMenuItem(page) {
-  document.querySelector("#menu").innerHTML += `
+    document.querySelector("#menu").innerHTML += `
   <a href="#${page.slug}" onclick="showPage('${page.slug}')">${page.title.rendered}</a>
   `;
 }
 
 // appends page section to the DOM
 function addPage(page) {
-  document.querySelector("#pages").innerHTML += `
+    document.querySelector("#pages").innerHTML += `
   <section id="${page.slug}" class="page">
     <header class="topbar">
-      <h2>${page.title.rendered}</h2>
+    <a href="index.html#forside">
+    <img src = "img/nubio-logo.svg">
+    </a>
     </header>
     ${page.content.rendered}
   </section>
@@ -98,10 +100,12 @@ function addPage(page) {
 }
 
 function addCategory(category) {
-  document.querySelector("#pages").innerHTML += `
+    document.querySelector("#pages").innerHTML += `
   <section id="${category.slug}" class="page">
     <header class="topbar">
-      <h2>${category.name}</h2>
+    <a href="index.html#forside">
+    <img src = "img/nubio-logo.svg">
+    </a>
     </header>
     <section class="container"></section>
   </section>
@@ -110,89 +114,132 @@ function addCategory(category) {
 
 // Fetches post data from headless cms
 function getMovies() {
-  fetch('http://rysholt.com/wordpress/wp-json/wp/v2/posts?_embed')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(movies) {
-      appendMovies(movies);
-    });
+    fetch('http://rysholt.com/wordpress/wp-json/wp/v2/posts?_embed')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (movies) {
+            appendMovies(movies);
+        });
 }
 
 // Appends json data to the DOM
 function appendMovies(movies) {
-  let htmlTemplate = "";
-  for (let movie of movies) {
-    console.log();
-    htmlTemplate += `
-      <article>
-      <img src="${getFeaturedImageUrl(movie)}">
+    let htmlTemplate = "";
+    for (let movie of movies) {
+        console.log();
+        htmlTemplate += `
+<div class="flip-container" onclick="this.classList.toggle('click');">
+<div class="flipper">
+      <article class="front">
+<img src = "${getFeaturedImageUrl(movie)}">
         <h3>${movie.title.rendered}</h3>
-        <p>Udgivelsesdato: ${movie.acf.udgivelses_dato}</p>
-        <p>Aldersgrænse: ${movie.acf.age_restriction}</p>
-        <p>Genre: ${movie.acf.genre}</p>
+        <h5>${movie.acf.subtitle}</h5>
+<div class="fronttitle">
+</div>
+</article>
+      <article class="back">
+<section class="secwhite">
         <iframe width="100% auto" height="auto" frameborder="0" allowfullscreen="allowfullscreen" gyroscope; picture-in-picture"
           src="https://youtube.com/embed/${movie.acf.trailer}">
         </iframe>
+<div class="fronttit">
+        <h3>${movie.title.rendered}</h3>
+        <h5>${movie.acf.genre}</h5>
+</div>
+<p>${movie.acf.description}</p>
+        </section>
+        <section class="backbutt">
+<h4>FAKTA</h4>
+<h4>MEDVIRKENDE</h4>
+<h4>ANMELDELSER</h4>
+<h4>BILLETTER</h4>
+</section>
       </article>
+        </div>
+        </div>
     `;
-  }
-  document.querySelector("#forside").innerHTML += htmlTemplate;
+    }
+    document.querySelector("#forside").innerHTML += htmlTemplate;
 }
 
 
 // Appends json data to the DOM
-function appendCategoriesMovies(movies,slug) {
-  let htmlTemplate = "";
-  for (let movie of movies) {
-    console.log();
-    htmlTemplate += `
-      <article>
-      <img src="${getFeaturedImageUrl(movie)}">
+function appendCategoriesMovies(movies, slug) {
+    let htmlTemplate = "";
+    for (let movie of movies) {
+        console.log();
+        htmlTemplate += `
+<div class="flip-container" onclick="this.classList.toggle('click');">
+<div class="flipper">
+      <article class="front">
+<img src = "${getFeaturedImageUrl(movie)}">
         <h3>${movie.title.rendered}</h3>
-        <p>${movie.content.rendered}</p>
+        <h5>${movie.acf.subtitle}</h5>
+<div class="fronttitle">
+</div>
+</article>
+      <article class="back">
+<section class="secwhite">
+        <iframe width="100% auto" height="auto" frameborder="0" allowfullscreen="allowfullscreen" gyroscope; picture-in-picture"
+          src="https://youtube.com/embed/${movie.acf.trailer}">
+        </iframe>
+<div class="fronttit">
+        <h3>${movie.title.rendered}</h3>
+        <h5>${movie.acf.genre}</h5>
+</div>
+<p>${movie.acf.description}</p>
+        </section>
+        <section class="backbutt">
+<h4>FAKTA</h4>
+<h4>MEDVIRKENDE</h4>
+<h4>ANMELDELSER</h4>
+<h4>BILLETTER</h4>
+        </section>
       </article>
+        </div>
+        </div>
     `;
-  }
-  document.querySelector("#"+ slug + " .container").innerHTML = htmlTemplate;
+    }
+    document.querySelector("#" + slug + " .container").innerHTML = htmlTemplate;
 }
 
 function getCategories() {
-  fetch('http://rysholt.com/wordpress/wp-json/wp/v2/categories')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(categories) {
-      appendCategories(categories);
-    });
+    fetch('http://rysholt.com/wordpress/wp-json/wp/v2/categories')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (categories) {
+            appendCategories(categories);
+        });
 }
 
 function appendCategories(categories) {
-  let htmlTemplate = "";
-  for (let category of categories) {
-    console.log(category);
-    addCategory(category)
-    htmlTemplate += `
+    let htmlTemplate = "";
+    for (let category of categories) {
+        console.log(category);
+        addCategory(category)
+        htmlTemplate += `
       <article>
         <h3 onclick="showCategory('${category.slug}','${category.id}')">${category.name}</h3>
       </article>
     `;
-  }
-  document.querySelector("#genrer").innerHTML += htmlTemplate;
+    }
+    document.querySelector("#genrer").innerHTML += htmlTemplate;
 }
 
-function showCategory(slug,id) {
-  console.log(slug,id);
-  showPage(slug);
+function showCategory(slug, id) {
+    console.log(slug, id);
+    showPage(slug);
 
-  fetch('http://rysholt.com/wordpress/wp-json/wp/v2/posts?_embed&categories='+id)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(movies) {
-    console.log(slug);
-    appendCategoriesMovies(movies,slug);
-  });
+    fetch('http://rysholt.com/wordpress/wp-json/wp/v2/posts?_embed&categories=' + id)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (movies) {
+            console.log(slug);
+            appendCategoriesMovies(movies, slug);
+        });
 }
 
 
@@ -200,20 +247,20 @@ function showCategory(slug,id) {
 
 // returns the source url of the featured image of given post or page
 function getFeaturedImageUrl(post) {
-  let imageUrl = "";
-  if (post._embedded['wp:featuredmedia']) {
-    imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
-  }
-  return imageUrl;
+    let imageUrl = "";
+    if (post._embedded['wp:featuredmedia']) {
+        imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
+    }
+    return imageUrl;
 }
 
 var myVar;
 
 function myFunction() {
-  myVar = setTimeout(showPageSpinner, 1500);
+    myVar = setTimeout(showPageSpinner, 1500);
 }
 
 function showPageSpinner() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "grid";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "grid";
 }
